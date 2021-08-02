779594eb51966c5fa850374ce989bdff04ad5df2
mengtianao <t5meng@uwaterloo.ca>
2021-08-01 23:36:46 -0400

    add some margin on footer

--- frontend/src/components/layouts/Footer.jsx
+++ frontend/src/components/layouts/Footer.jsx
@@ -3,7 +3,7 @@ import React, { Fragment } from "react";
 const Footer = () => {
   return (
     <Fragment>
-      <footer className="text-secondary text-center">
+      <footer className="text-secondary text-center mt-5">
         <p>E-Commercial - 2021-2022, All Rights Reserved by Tiano</p>
       </footer>
     </Fragment>
